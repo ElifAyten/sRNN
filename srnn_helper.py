15231f6e55a5c387e63aa08836c9233c8596d38e
ElifAyten <77199350+ElifAyten@users.noreply.github.com>
2025-10-28 17:58:22 +0100

    Update srnn_helper.py

--- srnn_helper.py
+++ srnn_helper.py
@@ -32,7 +32,7 @@ import matplotlib.pyplot as plt
 # =========================
 # Defaults (tune to Drive)
 # =========================
-DEFAULT_DATA_ROOT = Path("/content/drive/MyDrive")
+DEFAULT_DATA_ROOT = Path("/content/drive/MyDrive/rSLDS")
 DEFAULT_OUTPUTS_ROOT = Path("/content/drive/MyDrive/sRNN/sRNN-Model-Outputs")
 
 
@@ -56,9 +56,9 @@ def get_device() -> str:
 # =========================
 # Paths
 # =========================
-def h5_path_for_rat(rid: int, data_root: Path = DEFAULT_DATA_ROOT) -> Path:
+"""def h5_path_for_rat(rid: int, data_root: Path = DEFAULT_DATA_ROOT) -> Path:
     root = Path(data_root)
-    return root / "Rat-Data-hdf5" / f"Rat{rid}" / "NpxFiringRate_Behavior_SBL_10msBINS_0smoothing.hdf5"
+    return root / "Rat-Data-hdf5" / f"Rat{rid}" / "NpxFiringRate_Behavior_SBL_10msBINS_0smoothing.hdf5""""
 
 def csv_path_responsive_all(rid: int, data_root: Path = DEFAULT_DATA_ROOT) -> Path:
     root = Path(data_root)
